92920edf684506228489228947f39c2964862310
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2023-08-22 18:37:30 -0400

    feat(make): Create cookiecutter.mk

---
+++ src/platforms/cookiecutter.mk
@@ -0,0 +1,27 @@
+# cookiecutter.mk
+# .make
+#
+# Copyright © 2023 Dagitali LLC. All rights reserved.
+#
+# Facilitates Cookiecutter activities.
+# See https://cookiecutter.readthedocs.io/en/stable/installation.html.
+#
+# REFERENCES:
+# 1. https://www.gnu.org/prep/standards/html_node/Makefile-Conventions.html
+# 2. https://www.gnu.org/software/make
+# 3. https://web.mit.edu/gnu/doc/html/make_1.html
+
+
+# SECTION: FILE TARGETS ===================================================== #
+
+## cookiecutter: Install Cookiecutter.
+.PHONY: cookiecutter
+cookiecutter:
+ifeq (,$(shell which cookiecutter))
+	$(call msg,Installing command "$@")
+	ifeq (Darwin,$(uname))
+	@brew install $@
+	else
+	@pipx install $@
+	endif
+endif
