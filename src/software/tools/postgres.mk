abc63c6acd73cc8be7d7ecc53c134b8b4e55cbf3
Robert Jones <3848052+djrlj694@users.noreply.github.com>
2023-08-29 18:08:29 -0400

    feat(make): Create postgres.mk

---
+++ src/software/tools/postgres.mk
@@ -0,0 +1,46 @@
+# postgres.mk
+# .make
+#
+# Copyright © 2023 Dagitali LLC. All rights reserved.
+#
+# Abstracts command activities for PostgreSQL.
+# See https://www.postgresql.org/download/macosx.
+#
+# REFERENCES:
+# 1. https://www.gnu.org/prep/standards/html_node/Makefile-Conventions.html
+# 2. https://www.gnu.org/software/make
+# 3. https://web.mit.edu/gnu/doc/html/make_1.html
+
+
+# SECTION: INTERNAL VARIABLES =============================================== #
+
+# Command options
+postgres_opts = -h $(POSTGRES_HOST)
+postgres_opts += -U $(POSTGRES_USER)
+postgres_opts += -p $(POSTGRES_PORT)
+postgres_opts += -d $(POSTGRES_DB)
+
+
+# SECTION: PHONY TARGETS ==================================================== #
+
+## postgresql: Install PostgreSQL.
+.PHONY: postgresql
+postgresql:
+ifeq (,$(shell which psql))
+	$(call msg,Installing command "$@")
+ifeq (Darwin,$(uname))
+	@brew install $@@15
+endif
+endif
+
+## postgres-check: Checks PostgreSQL database server's connection status.
+.PHONY: postgres-check
+postgres-check:
+	$(call msg,Checking PostgreSQL database server's connection status)
+	pg_isready $(postgres_opts)
+
+## postgres-login: Connects to PostgreSQL database server via command line.
+.PHONY: postgres-login
+postgres-login:
+	$(call msg,Connecting to PostgreSQL database server via command line)
+	pgcli $(postgres_opts)
